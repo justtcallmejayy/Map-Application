f149ff2f4dd53390bfc8f8ee434af4978989d2fa
JAY CHOKSI <jaychokxii1804@gmail.com>
2025-02-14 10:49:58 -0500

    added links, few styling for map, logic from lecture notes.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+//StAuth10244: I Jay Choksi, 000887533 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
+
 // Logic script for the web-application of index file.
+
+// Basic map initialization
+let map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 6.5244, lng: 3.3792 },
+        zoom: 12,
+        mapId: "southern-guild-450915-f7"
+    });
+}
+  
