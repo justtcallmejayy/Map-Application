907ca709195388636cd5fa833a8dc1c1f1dcab6e
JAY CHOKSI <jaychokxii1804@gmail.com>
2025-02-14 17:41:37 -0500

    Added map function for initMap, also initializied makers array *10 locations.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //StAuth10244: I Jay Choksi, 000887533 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
 
-// Logic script for the web-application of index file.
-
-// Basic map initialization
+// Initialize the map, markers
 let map;
+let markers = [];
+
 function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 6.5244, lng: 3.3792 },
-        zoom: 12,
-        mapId: "southern-guild-450915-f7"
-    });
+  // Initialize the map
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 45.5244, lng: 90.3792 },
+    zoom: 12,
+    mapId: "southern-guild-450915-f7",
+  });
 }
-  
